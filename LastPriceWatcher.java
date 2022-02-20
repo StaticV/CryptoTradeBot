cfd64c678d530ebdfd6361911b4f6229e8996a71
StaticV <5557846+StaticV@users.noreply.github.com>
2022-02-20 11:27:11 -0500

    Add files via upload

--- LastPriceWatcher.java
+++ LastPriceWatcher.java
@@ -57,9 +57,7 @@ public class LastPriceWatcher extends PriceWatcher {
 		if (qty.compareTo(minOrder) == -1) return last;
 		
 		String order =  ex.orderMarketPrice(buy,coin,curr,qty.toPlainString(),test);
-		
-		System.out.print(new Date() + updown + dif.floatValue()*100 + "% $" + orderPrice);
-		System.out.print(" "+order);
+		System.out.print(new Date() + updown + dif.floatValue()*100 + "% $" + orderPrice+" "+order);
 		
 		l.updateLedger(ex);
 		l.buildTrades(curr);
